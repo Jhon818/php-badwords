a6b6bb5a4ab0e9bfbc95427b60b40ee635c85435
giov <pintogiovanni818@gmail.com>
2021-10-07 14:56:51 +0200

    init

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+    $text = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, nesciunt tempora dolor earum quis maxime illo esse delectus et quas doloribus ex cum temporibus voluptas vel inventore similique reiciendis qui!';
+ ?>
+
+ <h1>  <?php echo $text ?></h1>
